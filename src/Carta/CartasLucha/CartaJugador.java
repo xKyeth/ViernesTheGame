af68c4295d6bb9c95113bd49bf6e11829704e087
pecorte <pecorte@yahoo.es>
2019-04-09 08:45:46 +0200

    Cambios

---
+++ src/Carta/CartasLucha/CartaJugador.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Carta.CartasLucha;
+
+import Carta.Carta;
+
+/**
+ *
+ * @author pecorte
+ */
+public class CartaJugador extends Carta {
+    
+}
