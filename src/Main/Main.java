a6fba68c0a9782103f2a2c9a0f88eb5d128c69d9
usuario <usuario@DESKTOP-BLSQQIS>
2019-04-09 08:35:38 +0200

    Merge origin/master
    Conflicts:
    	.gitignore

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,6 +5,8 @@
  */
 package Main;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
 /**
  *
  * @author Imanol G
@@ -15,10 +17,44 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-        System.out.println("Hola Mundo");
-        
+       Scanner sn = new Scanner(System.in);
+        boolean salir = false;
+        int opcion; //Guardaremos la opcion del usuario
+ 
+        while (!salir) {
+ 
+            System.out.println("1. Comenzar Partida");
+            System.out.println("2. Cambiar Nivel de Dificultad");
+            System.out.println("3. Opcion 3");
+            System.out.println("4. Salir");
+ 
+            try {
+ 
+                System.out.println("Escribe una de las opciones");
+                opcion = sn.nextInt();
+ 
+                switch (opcion) {
+                    case 1:
+                        System.out.println("Has seleccionado la opcion 1");
+                        break;
+                    case 2:
+                        System.out.println("Has seleccionado la opcion 2");
+                        break;
+                    case 3:
+                        System.out.println("Has seleccionado la opcion 3");
+                        break;
+                    case 4:
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("Solo números entre 1 y 4");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Debes insertar un número");
+                sn.next();
+            }
+        }
+ 
     }
     
 }
