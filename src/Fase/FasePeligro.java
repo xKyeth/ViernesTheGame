aa95441c0366e9916b020790fb9b508076830a1b
usuario <usuario@DESKTOP-BLSQQIS>
2019-04-09 08:53:59 +0200

    Merge origin/master
    Conflicts:
    	.gitignore

--- src/Fase/FasePeligro.java
+++ src/Fase/FasePeligro.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Fase;
 
 /**
  *
- * @author alexandra
+ * @author Jesus Rosa
  */
 public class FasePeligro extends Fase {
+    
+    public FasePeligro(){
+        nombre = "Fase Peligro";
+    }
     
 }
