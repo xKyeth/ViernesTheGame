ddaa1352890e7aa45cf165562e79d7b61dfc4303
alexandra <alexandra@DAM15>
2019-04-11 12:07:21 +0200

    Merge origin/master
    Conflicts:
    	src/Carta/CartasLucha/CartaPeligro.java

--- src/Mazo/MazoCartasRobadas.java
+++ src/Mazo/MazoCartasRobadas.java
@@ -14,4 +14,18 @@ import java.util.LinkedList;
  */
 public class MazoCartasRobadas extends Mazo{
     LinkedList<Carta> listaCartasRobadas;
+    MazoPeligro peligro;
+    // Tengo que robar dos cartas, y este mazo tiene que mantener 1 de las
+    // dos cartas, la cual elige el usuario. (Este mazo va a servir como 
+    // tablero de las cartas)
+    
+    public void quedarseCarta(){
+        System.out.println("Robamos dos cartas.");
+        Carta carta1=peligro.robarCarta();
+        Carta carta2=peligro.robarCarta();
+        System.out.println("¿Qué carta quieres quedarte? 1 para la primera, 2 para la segunda.");
+        System.out.println(carta1);
+        System.out.println(carta2);
+        
+    }
 }
