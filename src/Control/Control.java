06ebdeb548c09d313213a38fa84046b53f609d2d
pecorte <pecorte@yahoo.es>
2019-04-29 08:42:45 +0200

    Cambios

--- src/Control/Control.java
+++ src/Control/Control.java
@@ -1,10 +1,41 @@
 package Control;
 
+import Carta.CartasLucha.CartaPeligro;
+import Estados.*;
+import Fase.*;
+import Jugador.Robinson;
+import Mazo.Descartes.*;
+import Mazo.*;
+import Vistas.Vista;
+
 /**
  *
  * @author Jesus Rosa
  */
 public class Control {
     // Para ese turno, robar una carta del mazo de Robinson y guardarlo en un arrayList de cartas para ese turno.
-    
+    Cambio cambio;
+    Robinson jugador;
+    Vista vista;
+    Fase fase;
+    CartaPeligro peligro;
+    Mazos mazos;
+    MazoDescartesPeligros descartesPeligro;
+    MazoDescartesRobinson descartesRobinson;
+    MazoDescartesTotal descartesFinal;
+    MazoCartasRobadas mano;
+    MazoEnvejecimiento mazoEnvejecimiento;
+    MazoPeligro mazoPeligro;
+    MazoPirata mazoPirata;
+    MazoRobinson mazoRobinson;
+    public Control(){
+        this.cambio=new EstadoVerde();
+        this.vista=new Vista(this);
+        this.fase=new FasePeligro();
+        this.mazos=new Mazos();
+        mazoEnvejecimiento=mazos.getMazoEnvejecimiento();
+        mazoPeligro=mazos.getMazoPeligro();
+        mazoPirata=mazos.getMazoPirata();
+        mazoRobinson=mazos.getMazoRobinson();
+    }
 }
---
+++ src/Vistas/Vista.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vistas;
+
+import Control.Control;
+
+/**
+ *
+ * @author pecorte
+ */
+public class Vista {
+    Control control;
+    public Vista(Control c){
+        this.control=c;
+    } 
+}
